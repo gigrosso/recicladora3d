857cfd43063edad69fd23554559e46213be82b27
Giovanni Grosso <giovannigrosso.contato@gmail.com>
2026-04-05 22:13:10 -0300

    revisão 6 do esquematico

---
+++ PCB/libarys/CD74HC4067.lplib.tmp/CD74HC4067-main/README.md
@@ -0,0 +1,68 @@
+# CD74HC4067
+CD74HC4067 16-Channel Analog Multiplexer/Demultiplexer for Wokwi<br /><br />
+Datasheet -> https://www.ti.com/lit/ds/symlink/cd74hc4067.pdf<br />
+Wokwi -> https://wokwi.com/<br /><br />
+
+The CD74HC4067 and CD74HCT4067 devices are digitally<br />
+controlled analog switches that utilize silicon-gate CMOS<br />
+technology to achieve operating speeds similar to LSTTL,<br />
+with the low power consumption of standard CMOS<br />
+integrated circuits.<br />
+These analog multiplexers/demultiplexers control analog<br />
+voltages that may vary across the voltage supply range.<br />
+They are bidirectional switches thus allowing any analog<br />
+input to be used as an output and vice-versa. The switches<br />
+have low “on” resistance and low “off” leakages. In addition,<br />
+these devices have an enable control which when high will<br />
+disable all switches to their “off” state.<br />
+
+<img src="https://i.imgur.com/J9hMKei.png" alt="Hardware" width="150" height="335">
+<img src="https://i.imgur.com/RcSO67U.png" alt="Simulation" width="150" height="392"><br /><br />
+<img src="https://i.imgur.com/wxUlurH.png" alt="Simulation" width="385" height="542"><br /><br /><br />
+
+<h2>Examples</h2>
+Multiplexing with potentiometers -> https://wokwi.com/projects/366560040454111233<br />
+Multiplexing with DIP switches -> https://wokwi.com/projects/367007283236072449<br />
+Driving LEDs -> https://wokwi.com/projects/366745528502490113<br />
+Demultiplexing with analog values -> https://wokwi.com/projects/366644917441189889<br /><br /><br />
+
+<h2>Attributes</h2>
+The CD74HC4067 has multiple attributes that can be configured via editing the diagram.json file.<br /><br />
+
+```
+{
+  "type": "board-cd74hc4067",
+  "id": "mux1",
+  "top": 103.19,
+  "left": 61.05,
+  "rotate": 90,
+  "attrs": { "digitalMux": "1" }
+  "attrs": { "analogDemux": "0" }
+  "attrs": { "digitalDemux": "0" }
+  "attrs": { "floatingSig": "0" }
+}
+```
+
+<h4>digitalMux</h4>
+If you want to use digitalRead() to read the SIG / COM pin when multiplexing with<br /> digital devices such as buttons, slide switches or dip switches, set the "digitalMux"<br />
+attribute to 1 in diagram.json. In this mode, the SIG / COM pin is a digital output.<br /><br />
+
+<h4>digitalDemux</h4>
+When used for demultiplexing digital values, set the "digitalDemux" attribute to 1.<br /> 
+In this mode, the logic level of the selected channel is equal to the logic<br /> level of the SIG / COM pin.
+Wokwi simulates LEDs with digital logic, so use this mode<br />
+if you want to use the multiplexer with LEDs.
+<br /><br />
+
+<h4>analogDemux</h4>
+When used for demultiplexing analog values, set the "analogDemux" attribute to 1.<br />
+
+This is only needed if you are connecting an analog device <br />
+such as a potentiometer to the SIG / COM pin on the CD74HC4067 chip.<br />
+In this mode, the voltage produced by the potentiometer is directed<br />
+to the selected channel of the CD74HC4067 chip.<br /><br />
+
+<h4>floatingSig</h4>
+If you want to connect multiple CD74HC4067 devices to the same ADC pin, set the<br />
+"floatingSig" attribute to 1 in diagram.json. In this mode, the SIG / COM pin is<br />
+in a high impedance state when the enable pin (E) is driven low.<br /><br />
---
+++ PCB/libarys/CD74HC4067.lplib.tmp/CD74HC4067-main/src/main.c
@@ -0,0 +1,227 @@
+// CD74HC4067
+// 16-Channel Analog Multiplexer
+// https://www.ti.com/lit/ds/symlink/cd74hc4067.pdf
+
+#include "wokwi-api.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+//! Channel pin names.
+const char* pin_names_I [16] = { 
+  "I0", "I1", "I2", "I3",
+  "I4", "I5", "I6", "I7", 
+  "I8", "I9", "I10", "I11",
+  "I12", "I13", "I14","I15"
+};
+
+//! Chip data.
+typedef struct {
+  pin_t  I[16];
+  pin_t  S[4];
+  pin_t  VCC;
+  pin_t  GND;
+  pin_t  COM;
+  pin_t  EN;
+  uint32_t digitalMux;
+  uint32_t floatingSig;
+  uint32_t analogDemux;
+  uint32_t digitalDemux;
+  timer_t timer;
+  uint16_t timer_interval;
+} chip_data_t;
+
+//! Returns true if VCC and GND are properly connected.
+bool power_connected(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+  return pin_read(chip->VCC) && !pin_read(chip->GND);
+}
+
+//! Called when E pin is high or the chip is not powered correctly.
+void off_state(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+
+  if (attr_read(chip->floatingSig))
+  {
+    pin_mode(chip->COM, INPUT);
+  }
+  else
+  {
+    pin_mode(chip->COM, ANALOG);
+    pin_dac_write(chip->COM, 0);
+  }
+    
+  for (int i = 0; i < 16; ++i)
+  {
+    pin_mode(chip->I[i], INPUT);
+  }
+}
+
+//! Returns the currently selected channel.
+uint8_t selected_channel(void *data)
+{
+    chip_data_t *chip = (chip_data_t*)data;
+    bool S0 = pin_read(chip->S[0]);
+    bool S1 = pin_read(chip->S[1]);
+    bool S2 = pin_read(chip->S[2]);
+    bool S3 = pin_read(chip->S[3]);
+    return S3 << 3 | S2 << 2 | S1 << 1 | S0;
+}
+
+//! Sets COM and all of the channel pins to analog mode.
+void analog_mux_mode(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+  pin_mode(chip->COM, ANALOG);
+      
+  for (int i = 0; i < 16; ++i)
+  {
+    pin_mode(chip->I[i], ANALOG);
+  }
+}
+
+//! Sets COM and all of the channel pins to digital mode.
+void digital_mux_mode(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+  pin_mode(chip->COM, OUTPUT);
+      
+  for (int i = 0; i < 16; ++i)
+  {
+    pin_mode(chip->I[i], INPUT);
+  }
+}
+
+//! Sets the selected channel to the same voltage read on the COM pin.
+void analog_demux(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+
+  for (int i = 0; i < 16; ++i)
+  {
+    pin_dac_write(chip->I[i], 0);
+  }
+  
+  uint8_t channel = selected_channel(chip);
+  pin_dac_write(chip->I[channel], pin_adc_read(chip->COM));
+}
+
+//! Sets the COM pin to the same logic level as the selected channel.
+void digital_demux(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+  if (chip->timer_interval != 1000)
+  {
+	  chip->timer_interval = 1000;
+	  timer_stop(chip->timer);
+	  timer_start(chip->timer, chip->timer_interval, true);  
+  }
+
+  for (int i = 0; i < 16; ++i)
+  {
+    pin_mode(chip->I[i], INPUT);
+  }
+  
+  uint8_t channel = selected_channel(chip);
+  pin_mode(chip->I[channel], OUTPUT);
+  pin_write(chip->I[channel], pin_read(chip->COM));
+}
+
+//! Sets the COM pin to the same voltage read on the selected channel.
+void mux(void *data)
+{
+  chip_data_t *chip = (chip_data_t*)data;
+  uint8_t channel = selected_channel(chip);
+  if (attr_read(chip->digitalMux))
+  {
+    pin_write(chip->COM, pin_read(chip->I[channel]));
+  }
+  else
+  {
+    pin_dac_write(chip->COM, pin_adc_read(chip->I[channel]));
+  }
+}
+
+//! Main loop.
+void chip_timer_callback(void *data) 
+{
+  chip_data_t *chip = (chip_data_t*)data;
+
+  if (power_connected(chip) && !pin_read(chip->EN))
+  {
+    pin_mode(chip->COM, INPUT_PULLDOWN);
+
+    if (attr_read(chip->digitalDemux))
+    {
+      digital_demux(chip);
+    }
+    else
+    {
+      if (chip->timer_interval != 100)
+      {
+        chip->timer_interval = 100;
+        timer_stop(chip->timer);
+        timer_start(chip->timer, chip->timer_interval, true);
+      }
+	  
+      if (attr_read(chip->digitalMux))
+      {
+        digital_mux_mode(chip);
+      }
+      else
+      {
+        analog_mux_mode(chip);  
+      }
+      
+      if (attr_read(chip->analogDemux))
+      {
+        analog_demux(chip);
+      }
+      else
+      {
+        mux(chip);
+      }
+    }
+  }
+  else
+  {
+    off_state(chip);
+  }
+}
+
+//! Chip initialization.
+void chip_init() 
+{
+  chip_data_t *chip = (chip_data_t*)malloc(sizeof(chip_data_t));
+
+  for (int i = 0; i < 16; ++i)
+  {
+    chip->I[i] = pin_init(pin_names_I[i], ANALOG);
+  }
+
+  chip->S[0] = pin_init("S0", INPUT);
+  chip->S[1] = pin_init("S1", INPUT);
+  chip->S[2] = pin_init("S2", INPUT);
+  chip->S[3] = pin_init("S3", INPUT);
+
+  chip->COM = pin_init("COM", INPUT);
+  chip->VCC = pin_init("VCC", INPUT);
+  chip->GND = pin_init("GND", INPUT);
+  chip->EN = pin_init("EN", INPUT);
+
+  chip->digitalMux = attr_init("digitalMux", 0);
+  chip->floatingSig = attr_init("floatingSig", 0);
+  chip->analogDemux = attr_init("analogDemux", 0);
+  chip->digitalDemux = attr_init("digitalDemux", 0);
+
+  const timer_config_t config = 
+  {
+    .callback = chip_timer_callback,
+    .user_data = chip,
+  };
+
+  chip->timer_interval = 100;
+  chip->timer = timer_init(&config);
+  timer_start(chip->timer, chip->timer_interval, true);
+}
